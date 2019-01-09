2ab40686c8b51af9b1a09ffad524263716de91d2
psl <pawel.slowik.dev@gmail.com>
2019-01-09 20:59:30 +0100

    Added logs and properties file in customer service

--- demo/customer-service/src/main/java/pl/com/psl/java/rx/demo/customerservice/CustomerController.java
+++ demo/customer-service/src/main/java/pl/com/psl/java/rx/demo/customerservice/CustomerController.java
@@ -1,6 +1,8 @@
 package pl.com.psl.java.rx.demo.customerservice;
 
-import org.springframework.http.MediaType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,16 +12,22 @@ import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+@CrossOrigin
 @RestController
 public class CustomerController {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(CustomerController.class);
 
     private static final Map<Long, Customer> CUSTOMERS = Stream.of(1L,2L,3L)
             .collect(Collectors.toMap(Function.identity(), (Long id) -> new Customer(id, "customer " + id)));
 
-    @GetMapping(value = "/customers/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping(value = "/customers/{id}")
     public Customer getCustomerSlowly(@PathVariable Long id) throws InterruptedException {
+        LOGGER.info("Received customer(id={}) request", id);
         Thread.sleep(5000);
-        return CUSTOMERS.get(id);
+        Customer customer = CUSTOMERS.get(id);
+        LOGGER.info("Returning customer(id={}) response", id);
+        return customer;
     }
 
 }
